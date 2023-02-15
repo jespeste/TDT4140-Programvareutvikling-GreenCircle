05fa0fc844a3b09a29da6d8bfb4d3d86d809955c
yololars <larsengoemonsen@gmail.com>
2023-02-15 10:26:43 +0100

    Fixed pocketbase import module-error

--- gc_app/app/posts/page.js
+++ gc_app/app/posts/page.js
@@ -1,27 +1,29 @@
 //"use client"
 import pb from '../lib/pocketbase';
 
-async function getPosts(){
-    const data = await pb.collection('posts').getList();
-
-    console.log(data);
-    console.log("Fetched");
-    return data.items; 
+async function getPosts() {
+	//try {
+	const data = await pb.collection('posts').getList();
+	console.log(data);
+	console.log('Fetched');
+	return data.items;
+	//} catch (err) {
+	//	console.log(err);
+	//}
 }
 
-export default async function PostsPage(){
-    const posts = await getPosts();
-    console.log(posts);
-    console.log("hello");
-    console.log(posts[0].name);
-    return (
-        <div>
-            <h1>Posts:</h1>
-            <ul>
-                {posts.map((post) => {
-                    return <li key={post.id}>{post.name}</li>
-                })}
-            </ul>
-        </div>
-    );
+export default async function PostsPage() {
+	const posts = await getPosts();
+	console.log(posts);
+	console.log('hello');
+	return (
+		<div>
+			<h1>Posts:</h1>
+			<ul>
+				{posts.map((post) => {
+					return <li key={post.id}>{post.name}</li>;
+				})}
+			</ul>
+		</div>
+	);
 }
