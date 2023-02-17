442b980d27a41b3cf7c21a881f5c7d160c6a16f7
Jesper Steinsrud <jespeste@stud.ntnu.no>
2023-02-17 10:52:00 +0100

    Added react-hook-form for login with pb

--- gc_app/app/user/page.js
+++ gc_app/app/user/page.js
@@ -1,5 +1,27 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
 
+export default function Page() {
+    const router = useRouter();
+    const { register, handleSubmit } = useForm();
 
-export default function User(){
-    return <p>User Page</p>
+    console.log(register('email'));
+
+    const handleClick = () => {
+            router.push('/');
+    }
+
+    return (<div>
+        <button type="button" onClick={handleClick}>
+        Click me
+        </button>
+        <form>
+            <input type="text" placeholder='email' />
+            <input type="text" placeholder='password' />
+
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    )
 }
