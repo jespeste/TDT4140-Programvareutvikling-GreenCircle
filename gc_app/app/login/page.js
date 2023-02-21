c6f09ed5b28752853e342f51f955b557e84d8ee3
yololars <larsengoemonsen@gmail.com>
2023-02-21 11:21:50 +0100

    design

--- gc_app/app/login/page.js
+++ gc_app/app/login/page.js
@@ -1,40 +1,64 @@
-"use client"
-import pb from "../lib/pocketbase"
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+'use client';
+import pb from '../lib/pocketbase';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './login.css';
 
 export default function Page() {
-    const [isLoading, setLoading] = useState();
-    const { register, handleSubmit } = useForm();
-    const router = useRouter();
+	const [isLoading, setLoading] = useState();
+	const { register, handleSubmit } = useForm();
+	const router = useRouter();
 
-    console.log(register('email'));
+	console.log(register('email'));
 
-    async function login(data){
-        setLoading(true);
-        try{
-            const authData = await pb.collection('users').authWithPassword(
-                data.email,
-                data.password,
-            );
-        } catch (e) {
-            alert(e);
-        }
-        setLoading(false);
-        if(pb.authStore.isValid){
-            router.push("/annonse");
-        }
-    }
+	async function login(data) {
+		setLoading(true);
+		try {
+			const authData = await pb.collection('users').authWithPassword(data.email, data.password);
+		} catch (e) {
+			alert(e);
+		}
+		setLoading(false);
+		if (pb.authStore.isValid) {
+			router.push('/annonse');
+		}
+	}
 
-    return (<div>
-        <h2>Logget inn: {pb.authStore.isValid.toString()}</h2>
-        {isLoading && <p>Loading</p>}
-        <form onSubmit={handleSubmit(login)}>
-            <input type="text" placeholder='email' {...register("email")} />
-            <input type="password" placeholder='password' {...register("password")}/>
-            <button type="submit" disabled={isLoading}>Login</button>
-        </form>
-        </div>
-    )
+	return (
+		<div className="root">
+			{/* <h2>Logget inn: {pb.authStore.isValid.toString()}</h2> */}
+			{isLoading && <p>Loading</p>}
+
+			<form onSubmit={handleSubmit(login)}>
+				<div className="container">
+					<h1>Logg inn</h1>
+					<div className="innercontainer">
+						<div className="username">
+							E-post:
+							<input type="text" placeholder="email" {...register('email')} />
+						</div>
+						<div className="password">
+							Passord:
+							<input type="password" placeholder="password" {...register('password')} />
+						</div>
+						<div>
+							<button className="loginbutton" type="submit" disabled={isLoading}>
+								Logg inn
+							</button>
+						</div>
+					</div>
+
+					<div className="innercontainer">
+						<div className="register">
+							Ikke bruker?
+							<button className="registerbutton" type="submit" disabled={isLoading}>
+								Lag profil
+							</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 }
