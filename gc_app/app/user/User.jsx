76d3f1c6f6fdf9efd091264aa4bc2a02a2baf4fd
yololars <larsengoemonsen@gmail.com>
2023-02-22 15:34:24 +0100

    finished for today

---
+++ gc_app/app/user/User.jsx
@@ -0,0 +1,37 @@
+'use client';
+import pb from '../lib/pocketbase';
+import './user.css';
+import Annonsecontainer from '../annonse/Annonsecontainer';
+
+function getUser() {
+	return pb.authStore.model;
+}
+
+async function getPostsFromUser(user_id) {
+	let posts = await pb.collection('posts').getList();
+}
+
+async function getPosts() {
+	const data = await pb.collection('posts').getList();
+	return data.items;
+}
+
+export default function User() {
+	pb.autoCancellation(false);
+	const user = getUser();
+	return (
+		<div className="root">
+			<div className="name">
+				{user.firstName} {user.lastName}
+			</div>
+			<div className="avatar">
+				<img src={user.avatar}></img>
+			</div>
+			{user.verified && <div className="verified"></div>}
+			<div className="email">{user.email}</div>
+			<div className="posts"></div>
+			<div className="favourites"></div>
+			{user.telephone == '' && <div className="telephone">{user.telephone} </div>}
+		</div>
+	);
+}
