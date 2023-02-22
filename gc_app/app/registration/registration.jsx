5d36bc6ca285b180019851bc1e772442017a9e1b
yololars <larsengoemonsen@gmail.com>
2023-02-22 08:12:19 +0100

    finished designing registration

--- gc_app/app/registration/registration.jsx
+++ gc_app/app/registration/registration.jsx
@@ -4,8 +4,9 @@ import User from './User';
 import { useState } from 'react';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import './registration.css';
 
-export default function Register() {
+export default function Registration() {
 	const [userName, setUserName] = useState('Username');
 	const [firstName, setFirstName] = useState('Name');
 	const [lastName, setLastName] = useState('Last name');
@@ -13,6 +14,7 @@ export default function Register() {
 	const [password, setPassword] = useState('Password');
 	const [passwordConfirm, setPasswordConfirm] = useState('Confirm Password');
 	const [telephone, setTelephone] = useState();
+	const [isLoading, setLoading] = useState();
 	const router = useRouter();
 
 	const handleSubmit = (event) => {
@@ -44,101 +46,106 @@ export default function Register() {
 	}
 
 	return (
-		<div className="maincontainer">
-			<h3>Lag bruker</h3>
-			<div className="centered">
-				<form onSubmit={handleSubmit}>
-					<div className="centereditems">
-						<div className="regcontainer">
-							<div className="inputfield">
-								Brukernavn:
-								<div>
-									<input
-										type="string"
-										value={userName}
-										onChange={(event) => setUserName(event.target.value)}
-										required
-									/>
+		<div>
+			{isLoading && <div className="loader"></div>}
+			{!isLoading && (
+				<div className="maincontainer">
+					<h3>Lag bruker</h3>
+					<div className="centered">
+						<form onSubmit={handleSubmit}>
+							<div className="centereditems">
+								<div className="regcontainer">
+									<div className="inputfield">
+										Brukernavn:
+										<div>
+											<input
+												type="string"
+												value={userName}
+												onChange={(event) => setUserName(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										Fornavn:
+										<div>
+											<input
+												type="string"
+												value={firstName}
+												onChange={(event) => setFirstName(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										Etternavn
+										<div>
+											<input
+												type="string"
+												value={lastName}
+												onChange={(event) => setLastName(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										Telefonnummer:
+										<div>
+											<input
+												type="tel"
+												value={telephone}
+												onChange={(event) => setTelephone(event.target.value)}
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										E-post:
+										<div>
+											<input
+												type="email"
+												value={email}
+												onChange={(event) => setEmail(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										Passord:
+										<div>
+											<input
+												type="password"
+												value={password}
+												onChange={(event) => setPassword(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
+									<div className="inputfield">
+										Gjenta passord:
+										<div>
+											<input
+												type="password"
+												value={passwordConfirm}
+												onChange={(event) => setPasswordConfirm(event.target.value)}
+												required
+											/>
+										</div>
+									</div>
 								</div>
+								<button type="submit" className="submitbutton" disabled={isLoading}>
+									Registrer bruker
+								</button>
 							</div>
-							<div className="inputfield">
-								Fornavn:
-								<div>
-									<input
-										type="string"
-										value={firstName}
-										onChange={(event) => setFirstName(event.target.value)}
-										required
-									/>
-								</div>
-							</div>
-							<div className="inputfield">
-								Etternavn
-								<div>
-									<input
-										type="string"
-										value={lastName}
-										onChange={(event) => setLastName(event.target.value)}
-										required
-									/>
-								</div>
-							</div>
-							<div className="inputfield">
-								Telefonnummer:
-								<div>
-									<input
-										type="tel"
-										value={telephone}
-										onChange={(event) => setTelephone(event.target.value)}
-									/>
-								</div>
-							</div>
-							<div className="inputfield">
-								E-post:
-								<div>
-									<input
-										type="email"
-										value={email}
-										onChange={(event) => setEmail(event.target.value)}
-										required
-									/>
-								</div>
-							</div>
-							<div className="inputfield">
-								Passord:
-								<div>
-									<input
-										type="password"
-										value={password}
-										onChange={(event) => setPassword(event.target.value)}
-										required
-									/>
-								</div>
-							</div>
-							<div className="inputfield">
-								Gjenta passord:
-								<div>
-									<input
-										type="password"
-										value={passwordConfirm}
-										onChange={(event) => setPasswordConfirm(event.target.value)}
-										required
-									/>
-								</div>
-							</div>
+						</form>
+						<div className="login">
+							Har du bruker?
+							<button type="button" className="logginbutton" onClick={login}>
+								Logg inn
+							</button>
 						</div>
-						<button type="submit" className="submitbutton">
-							Registrer bruker
-						</button>
 					</div>
-				</form>
-				<div className="login">
-					Har du bruker?
-					<button type="button" className="logginbutton" onClick={login}>
-						Logg inn
-					</button>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
