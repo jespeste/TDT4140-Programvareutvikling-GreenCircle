a9a7aab352a0bbbbd1a8939422ad482e81bde42c
yololars <larsengoemonsen@gmail.com>
2023-03-14 11:03:15 +0100

    fixed skrivefeil in "målevertøy"

--- gc_app/app/annonse/page.js
+++ gc_app/app/annonse/page.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import pb from '../lib/pocketbase';
 import Annonsecontainer from './Annonsecontainer';
 import Navbar from '../Navbar';
@@ -7,7 +7,7 @@ import { NativeSelect } from '@mantine/core';
 import './main.css';
 
 export default function Annonsepage() {
-	const [posts, setPostList] = useState([]); 
+	const [posts, setPostList] = useState([]);
 	const [search, setSearch] = useState('');
 	const [filter, setFilter] = useState('');
 	const [category, setCategory] = useState('');
@@ -18,7 +18,7 @@ export default function Annonsepage() {
 		{ value: 'Håndverktøy', label: 'Håndverktøy' },
 		{ value: 'Spikerpistol og kompressor', label: 'Spikerpistol og kompressor' },
 		{ value: 'Storelektrisk', label: 'Storelektrisk' },
-		{ value: 'Målevertøy', label: 'Målevertøy' },
+		{ value: 'Måleverktøy', label: 'Måleverktøy' },
 		{ value: 'Lim og festemidler', label: 'Lim og festemidler' },
 		{ value: 'Maling', label: 'Maling' },
 		{ value: 'Verktøyoppbevaring', label: 'Verktøyoppbevaring' }
@@ -26,40 +26,46 @@ export default function Annonsepage() {
 
 	const fetchPosts = async () => {
 		try {
-			const data = await pb.collection('posts').getList(1,100,{ 
-				'$autoCancel': true,
+			const data = await pb.collection('posts').getList(1, 100, {
+				$autoCancel: true,
 				expand: 'owner',
 				filter: `(title~"${search}" || description~"${search}") ${category}`
 			});
 			setPostList(data.items);
-		} catch (err){
+		} catch (err) {
 			console.log(err.isAbort);
 		}
-	}
+	};
 
-	useEffect(()=>{
-		if (filter !== ''){
+	useEffect(() => {
+		if (filter !== '') {
 			setCategory(`&& category="${filter}"`);
 		} else {
 			setCategory('');
 		}
 		fetchPosts();
-	}, [search, filter, category])
-	
+	}, [search, filter, category]);
+
 	return (
 		<div className="bigcontainer">
 			<Navbar></Navbar>
 			<div className="onerow">
-				<input type="text" value={search} onChange={(event) => setSearch(event.target.value)} className="searchbar" placeholder="Søk etter motorsag eller skrujern!" />
+				<input
+					type="text"
+					value={search}
+					onChange={(event) => setSearch(event.target.value)}
+					className="searchbar"
+					placeholder="Søk etter motorsag eller skrujern!"
+				/>
 				<NativeSelect
-						data={categories}
-						onChange={(event) => setFilter(event.currentTarget.value)}
-						value={filter}
-						radius="md"
-						size="47"
+					data={categories}
+					onChange={(event) => setFilter(event.currentTarget.value)}
+					value={filter}
+					radius="md"
+					size="47"
 				></NativeSelect>
 			</div>
-			<Annonsecontainer data={posts}/>
+			<Annonsecontainer data={posts} />
 		</div>
 	);
 }
