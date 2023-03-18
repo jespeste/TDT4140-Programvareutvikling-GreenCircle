1b98d236fd57d575717fab299d2dfccb33a947fd
magnsu <magnsu@ntnu.no>
2023-03-18 23:10:57 +0100

    Added darkmode.

--- gc_app/app/layout.js
+++ gc_app/app/layout.js
@@ -1,13 +1,17 @@
 /* eslint-disable @next/next/no-head-element */
 'use client';
 import './globals.css';
+import DarkMode from './DarkMode';
 
 export default function RootLayout({ children }) {
 	return (
 		<html>
 			<body>
-				<main>{children}</main>
+                <DarkMode prop={
+                    <main>{children}</main>
+                }/>
 			</body>
 		</html>
 	);
 }
+
