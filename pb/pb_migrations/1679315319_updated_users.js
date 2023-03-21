6b943aedb8b498b78b8ddb88eed2d4baf5f17232
yololars <larsengoemonsen@gmail.com>
2023-03-21 11:29:27 +0100

    positioning of loader

---
+++ pb/pb_migrations/1679315319_updated_users.js
@@ -0,0 +1,30 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "1trgockq",
+    "name": "blocked",
+    "type": "relation",
+    "required": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "maxSelect": null,
+      "displayFields": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("1trgockq")
+
+  return dao.saveCollection(collection)
+})
